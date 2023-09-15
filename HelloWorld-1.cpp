6123224d7f63d41d4069ddc39891ab8a073900fc
azer232 <ghoddoussi.reza@gmail.com>
2023-09-15 09:41:41 -0400

    Adding author name

--- HelloWorld-1.cpp
+++ HelloWorld-1.cpp
@@ -1,4 +1,6 @@
 // Simple C++ program to display "Hello World" 
+// Author: Mohammad-Reza Ghoddoussi <cp0953@wayne.edu> (I used my personal email on accident for this assignment)
+// My personal email is --- ghoddoussi.reza@gmail.com ---
 // Header file for input output functions 
 #include<iostream>  
   
